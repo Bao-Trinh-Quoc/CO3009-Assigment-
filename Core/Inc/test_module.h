f96020da415d14d43bab9a60fc38c1a9b54eae70
Cloudy <bao.trinhquoc@hcmut.edu.vn>
2024-11-22 08:04:00 +0700

    create unit test for each module prototype

---
+++ Core/Inc/test_module.h
@@ -0,0 +1,18 @@
+/*
+ * test_module.h
+ *
+ *  Created on: Nov 22, 2024
+ *      Author: DELL
+ */
+
+#ifndef INC_TEST_MODULE_H_
+#define INC_TEST_MODULE_H_
+
+#include "main.h"
+#include "button.h"
+
+void testMCU(void);
+void testButton(void);
+void testLED(void);
+
+#endif /* INC_TEST_MODULE_H_ */
---
+++ Core/Src/test_module.c
@@ -0,0 +1,16 @@
+/*
+ * test_module.c
+ *
+ *  Created on: Nov 22, 2024
+ *      Author: DELL
+ */
+
+void testMCU(void) {
+	HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
+}
+void testButton(void) {
+	// Add code to test here if needed
+}
+void testLED(void) {
+	// Add code to test here if needed
+}
